81a054c0a6fe3bdd91acc00919ca5e35f901a655
Leila Baghdadi <baghdadi@phenogenomics.ca>
2013-01-16 12:09:43 -0500

    changed handling tif files

--- GetOPTData/filehandle.h
+++ GetOPTData/filehandle.h
@@ -3,20 +3,25 @@
 #include <fstream>
 #include <dirent.h>
 #include <cstring>
+
 #include <vector>
 #include <algorithm>
 #include <cstdlib>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 using namespace std;
 
 bool cmdOptionExists(char** begin, char** end, const string& option);
 char* getCmdOption(char ** begin, char ** end, const string & option);
 bool compareFileNamesNumeric(const string& s1, const string& s2);
-string ExtractDirectory(string path);
-string ExtractFileName(string path);
-string getExtension(string path);
-bool getFilenameWithoutExtension(string path);
-string getPrefix(string path);
-int getDir (string dir, vector<string> &files,  vector<string> &darkfiles, string &logfile);
+string ExtractDirectory(const string path);
+string ExtractFileName(const string path);
+string getExtension(const string path);
+bool getFilenameWithoutExtension(const string path);
+string getPrefix(const string path);
+string getSuffix(const string path);
+int getDirbin(string dir, vector<string> &files,  vector<string> &darkfiles, string &logfile);
+int getDirtiff(string dir, vector<string> &files, string filenameprefix);
+
